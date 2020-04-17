827edf943c3567ce2f342e79b5389cb3ae83652a
Maco <Maco@LAPTOP-O1JLL8OB>
2020-04-18 01:00:50 +0200

    Completo

--- src/main/java/it/polito/tdp/quadrato/model/TestRisolviQuadrato.java
+++ src/main/java/it/polito/tdp/quadrato/model/TestRisolviQuadrato.java
@@ -5,10 +5,17 @@ import java.util.List;
 public class TestRisolviQuadrato {
 
 	public static void main(String args[]) {
-		RisolviQuadrato r = new RisolviQuadrato(3) ;
-		List<List<Integer>> soluzioni = r.risolvi() ;
+		RisolviQuadrato r = new RisolviQuadrato(4) ;
+		/*List<List<Integer>> soluzioni = r.risolvi() ;
 		for(List<Integer> sol: soluzioni)
-			System.out.println(sol) ;
+			System.out.println(sol) ;*/
+		//r.quadrati();
+		
+		List<List<Integer>>soluzioni=r.quadrati();
+		
+		for(List<Integer>sol : soluzioni) {
+			System.out.println(sol);
+		}
 	}
 	
 }
